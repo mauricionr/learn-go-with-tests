0ebf1e8b30aea9e7aab4f0e8e72b0a7b4ccb4c6c
mauricionr <mauricioskateboard@gmail.com>
2021-03-19 17:59:28 -0300

    finish hello

--- hello/hello.go
+++ hello/hello.go
@@ -2,15 +2,24 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string) string {
+
+func Hello(name string, languate string) string {
+
+		if languate == spanish {
+			return spanishHelloPrefix + name
+		}
+
 		if name == "" {
 			name = "World"
 		}
+
     return englishHelloPrefix + name
 }
 
 func main() {
-    fmt.Println(Hello("World"))
+    fmt.Println(Hello("World", ""))
 }
