68b0b4d0d3166adc4262e39d6fd5357239d84fb6
mauricionr <mauricioskateboard@gmail.com>
2021-03-22 21:00:14 -0300

    refactor table driven tests

--- structs_methods_interfaces/shapes.go
+++ structs_methods_interfaces/shapes.go
@@ -6,6 +6,15 @@ type Shape interface {
 	Area() float64
 }
 
+type Triangle struct {
+	Base float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
+
 type Rectangle struct {
 	Width float64
 	Height float64
