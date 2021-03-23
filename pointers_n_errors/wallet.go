89bba7988ab57615636987aa4d52e69ebc07c57c
mauricionr <mauricioskateboard@gmail.com>
2021-03-23 06:55:19 -0300

    refactor

--- pointers_n_errors/wallet.go
+++ pointers_n_errors/wallet.go
@@ -4,16 +4,27 @@ import "fmt"
 
 type Bitcoin int
 
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
 type Wallet struct {
 	balance Bitcoin
+}
+
+type Stringer interface {
+	String() string
 }
 
 func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
 
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
+}
+
 func (w *Wallet) Deposit(amount Bitcoin) {
-	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
 	w.balance += amount
 }
 
--- pointers_n_errors/wallet_test.go
+++ pointers_n_errors/wallet_test.go
@@ -2,16 +2,30 @@ package main
 
 import (
 	"testing"
-	"fmt"
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
-	got := wallet.Balance()
-	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
-	want := Bitcoin(10)
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
+
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
 	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+	})
 }
