90c7a49568ad889372b6e784f240f4c5a4a5dac5
mauricionr <mauricioskateboard@gmail.com>
2021-03-28 07:32:33 -0300

    handle unknow

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,11 +3,26 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
+
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
-	assertString(t, got, want)
+	t.Run("know word", func (t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+		assertString(t, got, want)
+	})
+
+	t.Run("unknow word", func (t *testing.T) {
+		_, err := dictionary.Search("unknow")
+		want := "could not find the word you are looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error")
+		}
+
+		assertString(t, err.Error(), want)
+	})
+
 }
 
 func assertString(t testing.TB, got, want string) {
