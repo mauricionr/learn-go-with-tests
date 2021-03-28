1564e7facf780a93bf097c9ec5ac8fc76ffbc322
mauricionr <mauricioskateboard@gmail.com>
2021-03-28 09:24:20 -0300

    Add

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -5,6 +5,12 @@ import "errors"
 type Dictionary map[string]string
 
 var ErrNotFound = errors.New("could not find the word you are looking for")
+var dictionary = make(map[string]string)
+
+
+func (d Dictionary) Add(word, definition string) {
+	d[word] = definition
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
